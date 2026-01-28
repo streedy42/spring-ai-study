ccdc1bbd5974e4058aca06990e7dff600c002ee3
streedy42 <pyujin42@gmail.com>
2026-01-28 22:32:11 +0900

    ai controller 추가

---
+++ src/main/java/com/example/demo/controller/AiController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@RestController
+@RequestMapping("/ai")
+@Slf4j
+public class AiController {
+  @PostMapping(
+    value="/chat",
+    consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+    produces = MediaType.TEXT_PLAIN_VALUE
+  )
+  public String chat(@RequestParam("question") String question) {
+      //TODO: process POST request
+      
+      return "아직 모델과 연결되지 않았습니다.";
+  }
+  
+}
