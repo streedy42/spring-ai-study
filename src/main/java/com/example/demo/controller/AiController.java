6acff45205f04f95910f6269094c37365b9bc834
streedy42 <pyujin42@gmail.com>
2026-02-01 09:52:56 +0900

    ch02. ChatModel stream

--- src/main/java/com/example/demo/controller/AiController.java
+++ src/main/java/com/example/demo/controller/AiController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.AiService;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 
 
@@ -40,6 +43,19 @@ public class AiController {
     return answerText;
   }
 
+  // ch02. 비동기 Chat-model 요청 매핑 메소드
+  @PostMapping(
+    value = "/chat-model-stream",
+    consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+    produces = MediaType.APPLICATION_NDJSON_VALUE
+  )
+  public Flux<String> chatModelStream(@RequestParam("question") String question) {
+    Flux<String> answerStreamText = aiService.generateStreamText(question);
+    return answerStreamText;
+  }
+  }
+  
+
 
 
   // public String chat(@RequestParam("question") String question) {
@@ -48,4 +64,4 @@ public class AiController {
   //     return "아직 모델과 연결되지 않았습니다.";
   // }
   
-}
+// }
--- src/main/java/com/example/demo/service/AiService.java
+++ src/main/java/com/example/demo/service/AiService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 
 @Service
 @Slf4j
@@ -51,6 +52,44 @@ public class AiService {
     String answer = assistantMessage.getText();
 
     return answer;
+
+  }
+
+  // 비동기 스트리밍 응답
+  public Flux<String> generateStreamText(String question) {
+        // 시스템 메시지 생성
+    SystemMessage systemMessage = SystemMessage.builder()
+        .text("사용자 질문에 대해 한국어로 답변을 해야 합니다.")
+        .build();
+
+    // 사용자 메시지 생성
+    UserMessage userMessage = UserMessage.builder()
+        .text(question)
+        .build();
+
+    // 대화 옵션 설정
+    ChatOptions chatOptions = ChatOptions.builder()
+        .model("gpt-4o")
+        .temperature(0.3)
+        .maxTokens(1000)
+        .build();
+
+    // 프롬프트 생성
+    Prompt prompt = Prompt.builder()
+        .messages(systemMessage, userMessage)
+        .chatOptions(chatOptions)
+        .build();
+    
+    // LLM에게 요청하고 응답받기. stream() 사용
+    Flux<ChatResponse> fluxResponse = chatModel.stream(prompt);
+    Flux<String> fluxString = fluxResponse.map(chatResponse -> {
+      AssistantMessage assistantMessage = chatResponse.getResult().getOutput();
+      String chunk = assistantMessage.getText();
+      if (chunk == null) chunk = "";
+      return chunk;
+    });
+
+    return fluxString;
 
   }
 
